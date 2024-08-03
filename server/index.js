23d0d0d60a46fb2026f54100303387934ef7866f
MuhammadDarab <darabmonib123@gmail.com>
2024-08-03 16:49:28 +0500

    backend domain changed.

--- server/index.js
+++ server/index.js
@@ -101,7 +101,7 @@ app.use(
       secure: true,
       maxAge: 1000 * 60 * 60 * 48,
       sameSite: 'none',
-      domain: 'https://cantt-college.vercel.app', // Adjust based on your domain
+      domain: 'https://arbitrary-elsi-test-orgs-3dafdc13.koyeb.app', // Adjust based on your domain
       path: '/', // Typically the root path
     }
   })
