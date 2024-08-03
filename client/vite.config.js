5bf3b013b9ba9dc9f68a51da64ade8b5a6416182
MuhammadDarab <darabmonib123@gmail.com>
2024-08-03 08:04:21 +0500

    Updating base

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Cantt-College-LMS/',
   plugins: [react()],
   build: {
-    outDir: '../docs'
+    outDir: '../docs',
   }
 })
